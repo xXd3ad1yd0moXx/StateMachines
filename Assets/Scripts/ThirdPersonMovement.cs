7b413318394ef9decc966d0b9598fb5e6ef15681
xXd3ad1yd0moXx <163761938+xXd3ad1yd0moXx@users.noreply.github.com>
2024-04-30 16:22:43 -0400

    fixed SimpleMove/Gravity issue

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -4,31 +4,57 @@ using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
-    CharacterController controller;
-    public float speed = 5;
+    public CharacterController controller;
+    public Transform cam;
+    public float speed = 10;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
-    public Transform cam;
 
-    private void Start()
+    private float verticalVelocity;
+    private float gravityValue = 9.81f;
+
+    Vector3 moveDir = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
     {
-        controller = GetComponent<CharacterController>();
+
     }
     // Update is called once per frame
     void Update()
     {
+        bool groundedPlayer = controller.isGrounded;
+
+        // slam into the ground
+        if (groundedPlayer && verticalVelocity < 0)
+        {
+            // hit ground
+            verticalVelocity = 0f;
+        }
+
+        verticalVelocity -= gravityValue * Time.deltaTime;
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if(direction.magnitude >= 0.1f)
+        if (direction.sqrMagnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.SimpleMove(moveDir.normalized  * speed * Time.deltaTime);
+            moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
         }
+        else
+        {
+            moveDir.x = 0f;
+            moveDir.z = 0f;
+        }
+
+        moveDir.y = verticalVelocity;
+
+        controller.Move(moveDir.normalized * speed * Time.deltaTime);
     }
 }
+
+    
